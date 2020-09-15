814955dfcad2f3d920d7bf789a776f77372e1a63
nine-zero-chan <90chanho@gmail.com>
2020-09-16 00:36:01 +0900

    component path

---
+++ packages/ds/component/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Header = void 0;
+var header_1 = require("./components/header");
+Object.defineProperty(exports, "Header", { enumerable: true, get: function () { return header_1.Header; } });
